d3e3d7ddcc1fe86ef92395845eb2b2993b334a0e
Ahmed Saber <38571794+DEV-A7med@users.noreply.github.com>
2019-01-22 20:25:16 +0200

    Initial Commit

---
+++ Kindle-Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kindle-Clone
+//
+//  Created by AHMED SR on 1/22/19.
+//  Copyright © 2019 AHMED SR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
